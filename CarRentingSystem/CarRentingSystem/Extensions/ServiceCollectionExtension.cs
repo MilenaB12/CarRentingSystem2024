606d9fb2163a7a3e13e05f9402418e032ef98d5b
milenaboyadzhieva <mb93212125@edu.mon.bg>
2024-03-13 16:09:05 +0200

    ServiceCollectionExtensions added

---
+++ CarRentingSystem/CarRentingSystem/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	public static class ServiceCollectionExtension
+	{
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddAplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<HouseDbContext>(options =>
+                options.UseSqlite(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+
+        public static IServiceCollection AddAplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireUppercase = false;
+            })
+            .AddEntityFrameworkStores<HouseDbContext>();
+
+            return services;
+        }
+    }
+}
+
