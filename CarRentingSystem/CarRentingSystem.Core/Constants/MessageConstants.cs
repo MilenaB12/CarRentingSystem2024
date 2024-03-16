40dde0b146651fd400f7d490c452c040910f69d5
milenaboyadzhieva <mb93212125@edu.mon.bg>
2024-03-16 15:13:06 +0200

    BecomeDealerModel created

---
+++ CarRentingSystem/CarRentingSystem.Core/Constants/MessageConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CarRentingSystem.Core.Constants
+{
+	public static class MessageConstants
+	{
+		public const string RequireMessage = "The {0} field is required";
+
+        public const string LengthMessage = "The field {0} must be between {2} and {1} characters long";
+
+    }
+}
+
--- CarRentingSystem/CarRentingSystem.Core/Models/Dealer/BecomeDealerFormModel.cs
+++ CarRentingSystem/CarRentingSystem.Core/Models/Dealer/BecomeDealerFormModel.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using static CarRentingSystem.Core.Constants.MessageConstants;
+using static CarRentingSystem.Infrastructure.Constants.EntityValidationConstants.Dealer;
 namespace CarRentingSystem.Core.Models.Dealer
 {
 	public class BecomeDealerFormModel
 	{
-
+		[Required(ErrorMessage = RequireMessage)]
+		[StringLength(PhoneNumberMaxLength,
+			MinimumLength = PhoneNumberMinLength,
+			ErrorMessage = LengthMessage)]
+		[Display(Name = "Phone number")]
+		[Phone]
+		public string PhoneNumber { get; set; } = null!;
 	}
 }
 
