ff9df51f4c5431e1e9d71fb7d05a48fce5eb6361
milenaboyadzhieva <mb93212125@edu.mon.bg>
2024-03-17 15:36:59 +0200

    NotADealer attribute added

---
+++ CarRentingSystem/CarRentingSystem/Attributes/NotADealerAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Claims;
+using CarRentingSystem.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CarRentingSystem.Attributes
+{
+	public class NotADealerAttribute : ActionFilterAttribute
+	{
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IDealerService? dealerService = context.HttpContext.RequestServices.GetService<IDealerService>();
+
+            if (dealerService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (dealerService != null
+                && dealerService.ExistsByIdAsync(context.HttpContext.User.Id()).Result)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+            }
+
+        }
+    }
+}
+
